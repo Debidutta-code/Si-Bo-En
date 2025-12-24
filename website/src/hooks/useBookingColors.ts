0e0715a05e7904ba92044c9a954a8dfbf4ff5a63
sksahilquotus <sk.sahil@quotus.co.in>
2025-12-24 11:18:31 +0530

    build ok

--- website/src/hooks/useBookingColors.ts
+++ website/src/hooks/useBookingColors.ts
@@ -6,11 +6,11 @@ export const useBookingColors = () => {
   const bookingColors = useSelector((state: RootState) => state.booking.bookingEngineColor) ;
 
   return {
-    primaryColor: bookingColors?.primaryColour || "#F97316",
-    secondaryColor: bookingColors?.secondaryColour || "#FB923C",
-    textColor: bookingColors?.TextColour || "#FFFFFF",
-    buttonBgColor: bookingColors?.ButtonbgColor || "#F97316",
-    buttonTextColor: bookingColors?.buttonTextColour || "#FFFFFF",
-    buttonHoverColor: bookingColors?.buttonHover || "#EA580C",
+    primaryColor: bookingColors?.primaryColor || "#F97316",
+    secondaryColor: bookingColors?.secondaryColor || "#FB923C",
+    textColor: bookingColors?.secondaryColor || "#FFFFFF",
+    buttonBgColor: bookingColors?.primaryColor || "#F97316",
+    buttonTextColor: bookingColors?.buttonTextColor || "#FFFFFF",
+    buttonHoverColor: bookingColors?.secondaryColor || "#EA580C",
   };
 };
