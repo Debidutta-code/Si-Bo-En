22bf670219eae1101c5c5a05b92843f0c4a5dedf
Debidutta <debiduttaacharya2002@gmail.com>
2026-02-27 17:04:41 +0530

    Revert "update payment to AED to USD in N-genius"
    This reverts commit e5fee8a93e61f05386725d0fd67a9b02cd3bb93d.

--- website/src/app/paymentCallback/page.tsx
+++ website/src/app/paymentCallback/page.tsx
@@ -77,7 +77,7 @@ const PaymentCallbackPage = () => {
               roomTypeCode: currentBooking.roomTypeCode,
               numberOfRooms: currentBooking.numberOfRooms || 1,
               finalPrice: currentBooking.finalPrice,
-              currency: currentBooking.finalPrice?.dailyBreakdown?.[0]?.currencyCode || "USD",
+              currency: currentBooking.finalPrice?.dailyBreakdown?.[0]?.currencyCode || "AED",
               email: currentBooking.email,
               phone: currentBooking.phone,
               guests: currentBooking.guests,
