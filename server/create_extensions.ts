9b94765cf23d23baa133fef6d2f0514e73f6ac37
Pritam-prasanjeet <162278058+Pritam-prasanjeet@users.noreply.github.com>
2026-05-11 12:18:15 +0530

    booking api for ota guest

--- server/create_extensions.ts
+++ server/create_extensions.ts
@@ -4,6 +4,9 @@ async function main() {
   try {
     await prisma.$executeRawUnsafe('CREATE EXTENSION IF NOT EXISTS pg_trgm;');
     await prisma.$executeRawUnsafe('CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;');
+    await prisma.$executeRawUnsafe(
+      'CREATE EXTENSION IF NOT EXISTS unaccent;'
+    );
     console.log("Extensions created successfully!");
   } catch (err) {
     console.error("Error creating extensions:", err);
