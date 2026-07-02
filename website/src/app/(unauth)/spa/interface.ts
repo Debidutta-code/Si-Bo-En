4bd5a33bcb03694460cb9ec114e9dd8700b5e608
Debidutta <debiduttaacharya2002@gmail.com>
2026-07-02 16:31:14 +0530

    refactor: remove duplicate additionalGuests property and IGuestSlot interface

--- website/src/app/(unauth)/spa/interface.ts
+++ website/src/app/(unauth)/spa/interface.ts
@@ -97,13 +97,4 @@ export interface ICreateSpaReservationRequest {
   additionalGuests?: IGuestSlot[];
   currencyCode: CurrencyCode;
   bookingCode?: string;
-  additionalGuests?: IGuestSlot[];
-}
-
-export interface IGuestSlot {
-  guestName: string;
-  guestEmail: string | null;
-  spaId: string;
-  slotsAvailableId: string;
-  amount: number;
 }
